55642a7a7076fc4731ac5def6426189282fcab4e
Jex <italwaysrainonme@gmail.com>
2014-10-24 18:13:57 +0800

    Finish !

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-golang_connect_to_mysql
+Golang connect to mysql
 =======================
 
-Golang connect to mysql
+# Install Mercurial
+
+    sudo apt-get install mercurial meld
+
+# Install mysql-server and golang-mysql driver
+
+    sudo apt-get install mysql-server
+    go get github.com/go-sql-driver/mysql
+
+# Create mysql data for testing
+
+    CREATE DATABASE go_test
+    CREATE TABLE tt (name char(10));
+    Insert tt values ('jex');
+    Insert tt values ('bob');
+    Insert tt values ('ted');
+
+# Execute `go run connect_to_mysql.go` and then you will see :
+
+    jex
+    bob
+    ted
---
+++ connect_to_mysql.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "database/sql"
+    _ "github.com/go-sql-driver/mysql"
+    "log"
+    "fmt"
+)
+
+func main() {
+    db, err := sql.Open("mysql", "root:@/go_test")
+    if err != nil {
+        panic(err.Error())
+    }
+    defer db.Close()
+
+    err = db.Ping()
+    if err != nil {
+        panic(err.Error())
+    }
+
+    rows, err := db.Query("SELECT * FROM tt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer rows.Close()
+    for rows.Next() {
+        var name string
+        if err := rows.Scan(&name); err != nil {
+            log.Fatal(err)
+        }
+        fmt.Printf("%s\n", name)
+    }
+    if err := rows.Err(); err != nil {
+        log.Fatal(err)
+    }
+}
+
+
