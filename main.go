83a921345fc0f8608dd1e172afc3e4ed45eb9776
Jex <italwaysrainonme@gmail.com>
2014-11-03 15:22:07 +0800

    Rename original to main.go

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:password@/go_test")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	rows, err := db.Query("SELECT * FROM tt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var name string
+		if err := rows.Scan(&name); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s\n", name)
+	}
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
